f70235cf59f9b45534851ce6d1bf919dec2c36d9
UDAYKUMAR P G <pguday4@gmail.com>
2023-12-07 22:54:10 +0530

    Create 1903. Largest Odd Number in String.java

---
+++ 2023 December Leetcoding Challenge/1903. Largest Odd Number in String.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int n = num.length();
+        int index = -1;
+        for(int i = n - 1; i >= 0; i--){
+            char c = num.charAt(i);
+            if(Character.valueOf(c) % 2 == 1){
+                index = i;
+                break;
+            }
+        }
+        return index == -1 ? "":num.substring(0, index + 1);
+    }
+}
+===================================================================================================
+class Solution {
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if (Character.getNumericValue(num.charAt(i)) % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        
+        return "";
+    }
+}
+
