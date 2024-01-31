24eee082a26af22ba4ef6e05391f1a53702f1831
UDAYKUMAR P G <pguday4@gmail.com>
2024-01-31 22:45:29 +0530

    Create 739. Daily Temperatures.java

---
+++ 2024 January Leetcoding Challenge/739. Daily Temperatures.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length , ans[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && temperatures[i] > temperatures[st.peek()]){
+                    ans[st.peek()] = i - st.pop();
+                }
+            st.push(i);
+        }
+        return ans;
+    }
+}
