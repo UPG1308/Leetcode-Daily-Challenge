83b294d746361938e903f269d595df62a29d2b1b
UDAYKUMAR P G <pguday4@gmail.com>
2024-05-16 22:48:56 +0530

    Create 2331. Evaluate Boolean Binary Tree.java

---
+++ 2024 May Leetcoding Challenge/2331. Evaluate Boolean Binary Tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root == null) return false;
+        if(root.val == 2){
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        else if(root.val == 3){
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+        return root.val == 1;
+    }
+}
