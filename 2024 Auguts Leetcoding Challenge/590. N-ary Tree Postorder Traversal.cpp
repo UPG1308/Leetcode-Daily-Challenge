72b7050182d345124042c6f529dfa6c9bbd79a0f
UDAYKUMAR P G <pguday4@gmail.com>
2024-08-26 21:11:53 +0530

    Create 590. N-ary Tree Postorder Traversal.cpp

---
+++ 2024 Auguts Leetcoding Challenge/590. N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> result;
+
+        // If the root is null, return the empty list
+        if (root == nullptr) return result;
+
+        stack<Node*> nodeStack;
+        nodeStack.push(root);
+
+        // Traverse the tree using the stack
+        while (!nodeStack.empty()) {
+            Node* currentNode = nodeStack.top();
+            nodeStack.pop();
+
+            result.push_back(currentNode->val);
+            // Push all the children of the current node to the stack
+            for (Node* child : currentNode->children) nodeStack.push(child);
+        }
+
+        // Reverse the result list to get the correct postorder traversal
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
