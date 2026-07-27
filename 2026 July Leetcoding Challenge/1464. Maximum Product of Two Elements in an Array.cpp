7fea081bb53f9689ec35c6ac84066537d6e6a775
UDAYKUMAR P G <pguday4@gmail.com>
2026-07-27 22:22:35 +0530

    Create 1464. Maximum Product of Two Elements in an Array.cpp

---
+++ 2026 July Leetcoding Challenge/1464. Maximum Product of Two Elements in an Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int mx1 = 0, mx2 = 0;
+        for(int num: nums){
+            if(num > mx1){
+                mx2 = mx1;
+                mx1 = num;
+            } else {
+                mx2 = max(mx2, num);
+            }
+        }
+        return (mx1 - 1) * (mx2 - 1);
+    }
+};
